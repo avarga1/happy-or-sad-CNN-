bc2c222b7e94cb4e745dbcbf94c5feca47f8facb
Austin Varga <64624232+avarga1@users.noreply.github.com>
2022-12-14 21:26:42 -0800

    CNN to detect facial expressions in png files

---
+++ c1w2.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.keras.preprocessing.image import load_img
+
+base_dir = "./data/"
+happy_dir = os.path.join(base_dir, "happy/")
+sad_dir = os.path.join(base_dir, "sad/")
+
+print("Sample happy image:")
+plt.imshow(load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}"))
+plt.show()
+
+print("\nSample sad image:")
+plt.imshow(load_img(f"{os.path.join(sad_dir, os.listdir(sad_dir)[0])}"))
+plt.show()
+from tensorflow.keras.preprocessing.image import img_to_array
+
+# Load the first example of a happy face
+sample_image  = load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}")
+
+# Convert the image into its numpy array representation
+sample_array = img_to_array(sample_image)
+
+print(f"Each image has shape: {sample_array.shape}")
+
+print(f"The maximum pixel value used is: {np.max(sample_array)}")
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy') is not None and logs.get('accuracy') > 0.999:
+            print("\nReached 99.9% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def image_generator():
+
+    from keras.preprocessing.image import ImageDataGenerator
+
+    image_data_generator = ImageDataGenerator()
+
+
+
+    train_datagen = ImageDataGenerator(rescale=1/255)
+    train_generator = train_datagen.flow_from_directory(
+        base_dir,
+        target_size=(150, 150),
+        batch_size=10,
+        class_mode='binary'
+    )
+    return train_generator
+
+from tensorflow.keras import optimizers, losses
+
+def train_happy_sad_model(train_generator):
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(loss=losses.binary_crossentropy ,
+                  optimizer=optimizers.RMSprop(lr=0.001),
+                  metrics=['accuracy'])
+
+    history = model.fit(
+        x = train_generator,
+        steps_per_epoch=2,
+        epochs=15,
+        verbose=1,
+        callbacks=[myCallback()]
+    )
+
+    return history
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
